98c8cd09dbb90614766259be481133323d45492b
Rijicp <32735767+Rijicp@users.noreply.github.com>
2017-10-13 13:37:51 +0530

    Update am.py

--- am.py
+++ am.py
@@ -5,15 +5,12 @@ import csv
 url='https://www.amazon.in/Adidas-Shoes/b?ie=UTF8&node=4510749031'
 data=requests.get(url).content
 
-
-
-
 file=open(r"/home/rijicp/output.txt","w")
 #file.write(datas)
 csv_writer=csv.writer(file)
 
-for i in data:
-	csv_writer.writerow(i)
-reads=csv.reader(file)
-print reads
+#for i in data:
+	#csv_writer.writerow(i)
+#reads=csv.reader(file)
+#print reads
 file.close()
